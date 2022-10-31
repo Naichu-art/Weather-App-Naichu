8b248c530c55211ae9de7fbdabe604cdb5927456
Naichu-art <75094034+Naichu-art@users.noreply.github.com>
2022-10-31 21:08:01 +1030

    Updated js

--- scr/app.js
+++ scr/app.js
@@ -1,1 +1,8 @@
-alert("hello world");
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
+let apiKey = "5bf3o6fa2c1bf4550542f2d00ata3a0c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
